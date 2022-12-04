f3b89b08767c7d928a973a73c942d5b4a50cf7a9
RossBrown2718 <rossbrown2718@gmail.com>
2022-12-03 17:05:19 -0800

    Added more comments

--- 2022 IAP-Turret/src/main/java/frc/robot/commands/TurnToAngle.java
+++ 2022 IAP-Turret/src/main/java/frc/robot/commands/TurnToAngle.java
@@ -4,37 +4,61 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.Turret;
+
 
 public class TurnToAngle extends CommandBase {
-  /** Creates a new TurnToAngle. */
-  public TurnToAngle(double angle) {
-    robot.turnTurettoPID(angle);
+
+  //Instantiates new variables
+  private Turret turret;
+  private double angle;
+  private Joystick joy;
+  private boolean teleOp;
+  private PIDController pid;
+
+  /* Creates a new TurnToAngle. */
+  public TurnToAngle(Turret turret, double angle, Joystick joy, boolean teleOp) {
     // Use addRequirements() here to declare subsystem dependencies.
+    this.turret = turret;
+    this.angle = angle;
+    this.joy = joy;
+    this.teleOp = teleOp;
+    addRequirements(turret); //Makes this command a part of the turret subsystem
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    pid = new PIDController(Constants.PIDConstants.kP, Constants.PIDConstants.kI, Constants.PIDConstants.kD); //defines the varibales used for PID
+    if(!teleOp) pid.setSetpoint(angle); //set point of the angle you want to turn to
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
-
-
-
-
-
-
-
+  public void execute() {
+    if(teleOp) {
+      turret.spin(-0.5 * joy.getY()); //Spins to the y position that the joystick is at
+      //-0.5 restricts the movement to not move too fast
+      SmartDashboard.putNumber("Joystick Power", joy.getY()); //Displays on SmartDashboard the y pos of the joystick
+    }
+    else turret.spin(pid.calculate(turret.getAngle())); //If in auto, spins to the given angle
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    turret.spin(0);
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return Math.abs(angle - turret.getAngle()) < Constants.TurretConstants.threshold;
+    //If the angle of the turret is close enough to the desired angle, calls the method to end (stop moving)
   }
 }
--- 2022 IAP-Turret/src/main/java/frc/robot/subsystems/Turret.java
+++ 2022 IAP-Turret/src/main/java/frc/robot/subsystems/Turret.java
@@ -13,58 +13,52 @@ import frc.robot.commands.TurnToAngle;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class Turret extends SubsystemBase {
-  private final WPI_TalonSRX  centerMotor;
-  private final WPI_TalonSRX motor;
-  private TurnToAngle tu;
-  private Joystick joy = new Joystick(0);
-  private Turret turret = new Turret();
-  /** Creates a new Turret. */
-  public Turret() {
-    motor = new WPI_TalonSRX(Constants.TurretConstants.talonPort);
+public class Turret extends SubsystemBase { //This is the class where the subsystem is run
+  private final WPI_TalonSRX centerMotor; //Instantiates a new motor
+  private final WPI_TalonSRX motor; //Instantiates a new motor
+  private Joystick joy = new Joystick(0); //Instantiates a new joystick
+  private Turret turret = new Turret(); //Instantiates a new turret
+  /* Creates a new Turret. */
+  public Turret() { //constrcutor header
+    motor = new WPI_TalonSRX(Constants.TurretConstants.talonPort); 
     motor.configFactoryDefault();
     motor.setInverted(false);
     motor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, 0, 10);
 
     centerMotor = new WPI_TalonSRX(Constants.TurretConstants.talonPort);
-  }
+    centerMotor.configFactoryDefault();
+    centerMotor.setInverted(false);
+    centerMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, 0, 10);
+  } // initiallizes the variables of the turret
 
-  public void setAngle(double angle){
+  public void setAngle(double angle){ //Defines the angle that the turret is at currently
     motor.setSelectedSensorPosition(angle);
   }
 
-  public double getAngle(){
+  public double getAngle(){ //Returns the angle the turret is at currently in degrees
     return motor.getSelectedSensorPosition() * 360 / (4096.0);
+    //4096 is the number of ticks in a circle,
+    //so to convert from ticks to degrees
+    //multiplying by 360 and dividing by 4096 converts it
   }
 
-  public double getActualAngle()
-  {    
+  public double getActualAngle(){ //Returns the angle the turret should be at in degrees
     return centerMotor.getSelectedSensorPosition() * 360 / (4096.0);
   }   
 
-  public void spin(double speed)
-  {
+  public void spin(double speed){ //Makes the motor spin some speed from 0 to 1
     motor.set(ControlMode.PercentOutput, speed);
   }
 
-  public double findSlippageAngle()
-  {
+  public double findSlippageAngle(){ //Returns the angle that the robot slipped in degrees
     return getActualAngle() - getAngle();
   }
 
-  public    void    correctSlippage()
-  { 
-    setAngle(getActualAngle() -     findSlippageAngle());
-    turnToAngle(getAngle());
-  }
-
-  private void turnToAngle(double angle) {
-    tu = new TurnToAngle(turret, angle, joy, true);
-  }
-
   @Override
-  public void periodic() {
+  public void periodic() { //Displays the angle the turrent is at on smartDashboard
     SmartDashboard.putNumber("Angle: ", getAngle());
+    SmartDashboard.putNumber("Actual Angle: ", getActualAngle());
+    SmartDashboard.putNumber("Angle Slipped: ", findSlippageAngle());
     //SmartDashboard.putNumber("Joystick Power: ", RobotContainer.getJoy().getY());
   }
 }
